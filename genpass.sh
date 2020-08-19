420284da2431c02bf62ba27c29a1ff10d4fd1fd3
Maigurs <31n81t@gmx.com>
2020-08-19 10:21:44 +0100

    Update genpass.sh

--- genpass.sh
+++ genpass.sh
@@ -12,8 +12,8 @@ cat tmp.txt >> secrets.txt && \
 apg -a 0 -n 10 -m 96 > tmp.txt && \
 apg -a 1 -n 10 -m 96 >> tmp.txt && \
 cat tmp.txt >> secrets.txt && \
+cat secrets.txt && \
 mv secrets.txt secrets`date +%S%M`.txt && \
-cat tmp.txt && \
 rm tmp.txt && \
 ccrypt -e *.txt && \
 exit
