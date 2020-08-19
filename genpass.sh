008656479b680480aabb19aec83a16850afd1e0e
Maigurs <31n81t@gmx.com>
2020-08-19 09:53:51 +0100

    Create genpass.sh

---
+++ genpass.sh
@@ -0,0 +1,19 @@
+touch tmp.txt && \
+touch secrets.txt && \
+apg -a 0 -n 10 -m 12 > tmp.txt && \
+apg -a 1 -n 10 -m 12 >> tmp.txt && \
+cat tmp.txt > secrets.txt && \
+apg -a 0 -n 10 -m 24 > tmp.txt && \
+apg -a 1 -n 10 -m 24 >> tmp.txt && \
+cat tmp.txt >> secrets.txt && \
+apg -a 0 -n 10 -m 48 > tmp.txt && \
+apg -a 1 -n 10 -m 48 >> tmp.txt && \
+cat tmp.txt >> secrets.txt && \
+apg -a 0 -n 10 -m 96 > tmp.txt && \
+apg -a 1 -n 10 -m 96 >> tmp.txt && \
+cat tmp.txt >> secrets.txt && \
+mv secrets.txt secrets`date +%S%M`.txt && \
+cat tmp.txt && \
+rm tmp.txt %% \
+ccrypt -e *.txt && \
+exit
