e8c1c0cf0157008226ffb172d8d1fc14c6a53d13
Maigurs <31n81t@gmx.com>
2020-09-07 18:07:12 +0100

    Update genpass.sh
    Compacting code-base by removing unnecessary characters and symbols.

--- genpass.sh
+++ genpass.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-touch tmp.txt && \
-touch secrets.txt && \
-apg -a0 -n10 -m12 > tmp.txt && \
-apg -a1 -n10 -m12 >> tmp.txt && \
-cat tmp.txt > secrets.txt && \
-apg -a0 -n10 -m24 > tmp.txt && \
-apg -a1 -n10 -m24 >> tmp.txt && \
-cat tmp.txt >> secrets.txt && \
-apg -a0 -n10 -m48 > tmp.txt && \
-apg -a1 -n10 -m48 >> tmp.txt && \
-cat tmp.txt >> secrets.txt && \
-apg -a0 -n10 -m96 > tmp.txt && \
-apg -a1 -n10 -m96 >> tmp.txt && \
-cat tmp.txt >> secrets.txt && \
-cat secrets.txt && \
-mv secrets.txt secrets`date +%d%H%M%S`.txt && \
-rm tmp.txt && \
-ccrypt -e *.txt && \
+touch tmp.txt
+touch secrets.txt
+apg -a0 -n10 -m12 > tmp.txt
+apg -a1 -n10 -m12 >> tmp.txt
+cat tmp.txt > secrets.txt
+apg -a0 -n10 -m24 > tmp.txt
+apg -a1 -n10 -m24 >> tmp.txt
+cat tmp.txt >> secrets.txt
+apg -a0 -n10 -m48 > tmp.txt
+apg -a1 -n10 -m48 >> tmp.txt
+cat tmp.txt >> secrets.txt
+apg -a0 -n10 -m96 > tmp.txt
+apg -a1 -n10 -m96 >> tmp.txt
+cat tmp.txt >> secrets.txt
+cat secrets.txt
+mv secrets.txt secrets`date +%d%H%M%S`.txt
+rm tmp.txt
+ccrypt -e *.txt
 exit
