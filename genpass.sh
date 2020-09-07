0057d41ecb765c154fd101e28302c1b481512eb6
Maigurs <31n81t@gmx.com>
2020-09-07 18:03:48 +0100

    Update genpass.sh
    Compacting code-base by removing unnecessary spaces between arguments and their respective numeric values.

--- genpass.sh
+++ genpass.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 touch tmp.txt && \
 touch secrets.txt && \
-apg -a 0 -n 10 -m 12 > tmp.txt && \
-apg -a 1 -n 10 -m 12 >> tmp.txt && \
+apg -a0 -n10 -m12 > tmp.txt && \
+apg -a1 -n10 -m12 >> tmp.txt && \
 cat tmp.txt > secrets.txt && \
-apg -a 0 -n 10 -m 24 > tmp.txt && \
-apg -a 1 -n 10 -m 24 >> tmp.txt && \
+apg -a0 -n10 -m24 > tmp.txt && \
+apg -a1 -n10 -m24 >> tmp.txt && \
 cat tmp.txt >> secrets.txt && \
-apg -a 0 -n 10 -m 48 > tmp.txt && \
-apg -a 1 -n 10 -m 48 >> tmp.txt && \
+apg -a0 -n10 -m48 > tmp.txt && \
+apg -a1 -n10 -m48 >> tmp.txt && \
 cat tmp.txt >> secrets.txt && \
-apg -a 0 -n 10 -m 96 > tmp.txt && \
-apg -a 1 -n 10 -m 96 >> tmp.txt && \
+apg -a0 -n10 -m96 > tmp.txt && \
+apg -a1 -n10 -m96 >> tmp.txt && \
 cat tmp.txt >> secrets.txt && \
 cat secrets.txt && \
 mv secrets.txt secrets`date +%d%H%M%S`.txt && \
